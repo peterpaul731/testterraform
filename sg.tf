34280d2d8ea3c2efdd5410d490e9af809a62beaa
peter <peterpaul731@gmail.com>
2022-02-16 17:59:12 +0530

    Third commit

--- sg.tf
+++ sg.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   version = "=2.95.0"
+  subscription_id = "d5b3f059-9021-498a-85b1-88fe5ed0a6e7"
+  client_id       = "c10e96c0-4387-4ba5-bfde-f954da605882"
+  client_secret   = "lrZlQUVuM-e~Eu2xGeDZhhkqUpxIR73n.-"
+  tenant_id       = "9f6dfea8-7e92-4eb9-86d6-b6b967ecc2a2"
+}
   features {}
 }
 data "azurerm_resource_group" "example" {
