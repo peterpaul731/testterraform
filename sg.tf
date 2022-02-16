6c3d9ef793f421621f6868c3f5fd79068b3a9918
peter <peterpaul731@gmail.com>
2022-02-16 17:24:30 +0530

    second commit

--- sg.tf
+++ sg.tf
@@ -1,23 +1,29 @@
+provider "azurerm" {
+  version = "=2.95.0"
+  features {}
+}
 data "azurerm_resource_group" "example" {
   name     = "newgrp1"
 }
  
-resource "azurerm_network_security_group" "testgrp" {
-  name                = "acceptanceTestSecurityGroup2"
-  location            = $(data.azurerm_resource_group.example.location)
-  resource_group_name = $(data.azurerm_resource_group.example.name)
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 }
  
-resource "azurerm_network_security_rule" "testgrp" {
-  name                        = "testgrp-out"
+ 
+resource "azurerm_network_security_rule" "example" {
+ 
+  name                        = "test234"
   priority                    = 100
-  direction                   = "Outbound"
+  direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "*"
+  destination_port_range      = "80-443"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = $(data.azurerm_resource_group.example.name)
-  network_security_group_name = azurerm_network_security_group.testgrp.name
+  resource_group_name         = data.azurerm_resource_group.example.name
+  network_security_group_name = azurerm_network_security_group.example.name
 }
